09627e92cfab6002b0af37a2a615a08188855a8b
2azahar786 <2azahar786@gmail.com>
2022-12-14 02:02:04 +0530

    initial commit

---
+++ index_script.js
@@ -0,0 +1,162 @@
+// Conditions
+
+// if else
+// switch case
+// ternary operator/ tarnary conditions
+
+// Rating mini app
+// let rating=4;
+
+// if (rating==5){
+//     console.log('5 stars');
+// }
+
+// else if (rating==4){
+//     console.log('4 stars');
+// }
+
+// else if(rating==3){
+//     console.log('3 stars');
+// }
+
+// else if (rating==2){
+//     console.log('2 stars');
+// }
+
+// else {
+//     console.log('I am else');
+// }
+
+
+// switch
+
+// let user ='admin';
+
+// switch (user) {
+//     case 'admin':
+//     console.log('Redirect him/her to admin page');
+//     // break;
+
+//     case 'mentor':
+//         console.log('Redirect him/her to mentor page');
+//         // break;
+     
+//     case 'principal':
+//         console.log('Redirect him/her to principal page');
+//         // break;
+        
+//         default:
+//             console.log('I am simple user');
+// }
+
+
+// Ternary Operator / Ternary conditions
+
+// conditions ? true : false
+
+// let userlogin = false;
+
+// userlogin ? console.log('logout') : console.log('login');
+
+// condition ?  true  : false
+
+
+// let name = [
+//     'Azhar',
+//     'deepak',
+//     'Sumant',
+//     'Serajul',
+//     'Rakesh',
+//     'Dhananajay',
+//     'Nooralam',
+//     'Tipu',
+// ];
+
+// console.log(name);
+// console.log(name[7]);
+
+// Object
+
+// let bankaccount = {
+//     firstname : 'Azhar',
+//     Lastname : 'Ansari',
+//     mobileno : 7506976882,
+//     balance : 9999999,
+// };
+
+// console.log(bankaccount);
+// console.log(bankaccount.balance);
+
+
+// Date and Math
+
+const now = new Date();
+
+// console.log(now);
+// console.log(now.toString());
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDay());
+// console.log(now.getUTCMonth());
+
+// console.log(now.getTime());
+
+// console.log(now.getHours());
+// console.log(now.getMinutes());
+// console.log(now.getSeconds());
+
+
+// Math
+
+// const  PI = Math;
+// console.log(PI);
+
+// console.log(Math.PI); 
+// console.log(Math.round(PI));
+// console.log(Math.round(11.2));
+// console.log(Math.floor(10.5));
+// console.log(Math.ceil(10.5));
+
+// Min and Max
+
+// console.log(Math.min(5,3,10,8,44,10,11,6,));
+// console.log(Math.max(44,22,5,6,55,88,21,28,29,101));
+  
+
+// Random
+
+// console.log(Math.random());  // 0 - 0.999999999 / (0 - 1)
+// console.log(Math.round(Math.random()));
+// console.log(Math.round(Math.random()*51));
+
+
+// console.log(Math.pow(3, 3));
+// console.log(Math.pow(2, 4));
+// console.log(Math.log(3));
+// // console.log(Math.log(9));
+// console.log(Math.sin(90));
+// console.log(Math.cos(45));
+
+
+// Loops
+// Do while, While, for
+
+// let i=5;
+
+// do {
+//     console.log('value of i is :', i);
+//     i++;
+// } while (i<=8);
+
+
+// let j=8;
+
+// while (j<=12){
+//     console.log('value of j is', j);
+//     j++;
+// }
+
+//  for (let i=2; i<=8; i++){
+    // console.log('value of i is ', i);
+    // console.log(i);
+//  }
---
+++ readme.md
@@ -0,0 +1,38 @@
+# Full Stack Javascript Bootcamp 3.0
+
+## This lecture is for conditions, date and Math, Loops
+
+**For rmore clarity on the javascript basic read index_script.js file**
+
+>Loops => Do while, While, for
+
+
+_Do while_
+```javascript
+let i=5;
+
+do {
+    console.log('value of i is :', i);
+    i++;
+} while (i<=8);
+```
+
+_While_
+```Javascript
+let j=8;
+
+while (j<=12){
+    console.log('value of j is', j);
+    j++;
+}
+```
+
+_For_
+```javascript
+ for (let i=2; i<=8; i++){
+    console.log('value of i is ', i);
+    console.log(i);
+ }
+```
+
+[Click Here to see th full javascript basics](./index_script.js)
